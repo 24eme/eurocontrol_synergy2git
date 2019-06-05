cf4cea36472c011d25a9b134737d94a67174a8f7
Tangui Morlier <tangui@tangui.eu.org>
2019-06-05 20:44:30 +0200

    Retrieve deletion tasks and convert them to git commits

--- subadd.sh
+++ subadd.sh
@@ -26,10 +26,19 @@ if ! test "$alltasks" ; then
 fi
 
 # the second column of git status output is the list of modified, created or deleted files
-# the file /tmp/subadd.$$.tmp will contain the md5sum of all the files to be modified or created followed by spaces, then their full pathname.
-git status -s | awk '{print $2}' | while read path ; do
-	find $path -type f -exec md5sum '{}' ';' 
-done > /tmp/subadd.$$.tmp
+# the file /tmp/subadd_content.$$.tmp will contain the md5sum of all the files to be modified or created followed by spaces, then their full pathname.
+git status -s > /tmp/subadd_status.$$.tmp
+
+# retrieve content of each Modification
+cat /tmp/subadd_status.$$.tmp | grep -v " D " | awk '{print $2}' | while read path ; do
+	find $path -type f -exec md5sum '{}' ';'
+done > /tmp/subadd_content.$$.tmp
+# retrieve content of deletion (to retrieve all missing version between the deleted content and the current git content)
+cat /tmp/subadd_status.$$.tmp | grep " D " | awk '{print $2}' | while read path ; do
+	deletion_md5=$(git cat-file -p $(git ls-tree HEAD "$path" | cut -d " " -f 3 | sed -r 's/^([a-z0-9]+).+$/\1/')  | md5sum | sed 's/ *-.*//')
+	printf "$deletion_md5  $path deletion"
+done >> /tmp/subadd_content.$$.tmp
+
 
 # save all the changes to the stash. This revert the working tree to the last commit
 git stash > /dev/null 
@@ -37,8 +46,8 @@ git stash > /dev/null
 # ensure we have write access
 chmod -R u+w .
 
-# for each md5sum and pathname in /tmp/subadd.$$.tmp
-cat /tmp/subadd.$$.tmp | while read md5 path extra ; do 
+# for each md5sum and pathname in /tmp/subadd_content.$$.tmp
+cat /tmp/subadd_content.$$.tmp | while read md5 path extra ; do
 	# use the md5sum to retrieve the objectname
 	echo -n "$path;"; grep $md5 $objmd5  | head -n 1
 	echo ; 
@@ -76,9 +85,22 @@ done | grep ';[a-f0-9]' | sed 's/;/ /g' | while read path newmd5 newid; do
 	        grep "$newid" $allobj | head -n 1
 	        echo ;
 	fi
-# foreach create_time, owner, task, path and md5sum (sorted by date). A dummy line "#_FIN_" is read at the last iteration.
-done | awk -F ';' '{print $8" "$6" "$7" "$1" "$2}END{print "#_FIN_ "}' | grep '_' | sort | while read date auteur task path md5; do 
-	# if the task changes (or it is the #_FIN_), commit the previous group of changes
+done > /tmp/subadd_tasks.$$.tmp
+
+#retrieve deletion task
+cat /tmp/subadd_status.$$.tmp | grep " D " | awk '{print $2}' | while read path ; do
+	deletion_md5=$(git cat-file -p $(git ls-tree HEAD "$path" | cut -d " " -f 3 | sed -r 's/^([a-z0-9]+).+$/\1/')  | md5sum | sed 's/ -.*//')
+	deletion_id=$(grep $deletion_md5 $objmd5 | sed 's/.*;//' | tail -n 1)
+	deletion_dirpath=$(find $path2dump"/files/" -name '*dir' -exec grep -l "\- $deletion_id" '{}' ';')
+	deletion_dirid=$(head -n 1 $deletion_dirpath | sed 's/^= //' | sed 's/ =$//')
+	deletion_dirmd5=$(grep $deletion_dirid $objmd5 | tail -n 1 | awk -F ';' '{print $1}')
+	echo -n $path";"$deletion_dirmd5";" ;
+	grep "$deletion_dirid" $allobj | head -n 1
+done >> /tmp/subadd_tasks.$$.tmp
+
+# foreach create_time, owner, task, path and md5sum (sorted by date). A dummy line "#_END_" is read at the last iteration.
+cat /tmp/subadd_tasks.$$.tmp | awk -F ';' '{print $8" "$6" "$7" "$1" "$2}END{print "#_END_ "}' | grep '_' | sort | while read date auteur task path md5; do
+	# if the task changes (or it is the #_END_), commit the previous group of changes
 	if test "$old" && ! test "$old" = "$auteur $task"; then
 		if ! test "$mycomment" ; then
 			mycomment="commentaire vide: $mytask"
@@ -88,10 +110,16 @@ done | awk -F ';' '{print $8" "$6" "$7" "$1" "$2}END{print "#_FIN_ "}' | grep '_
 	if test "$path" && test "$md5"; then
 		# perform the file change or creation in the working tree
 		path2md5file=$path2dump"/files/"$(echo $md5 | sed 's/\(..\)\(..\)/\1\/\2\//')
-		mkdir -p $(dirname $path)
-		zcat $path2md5file > $path
+		#retrieve the content if exists
+		if test -f "$path2md5file" ; then
+			mkdir -p $(dirname "$path")
+			zcat $path2md5file > "$path"
+		#otherwise if there is a .dir file, it's a deletion
+		elif test -f $path2md5file".dir" ; then
+			rm "$path"
+		fi
 		# stage the file for the next commit
-		git add $path
+		git add --all "$path"
 	fi
 	old="$auteur $task"
 	mytask=$task
@@ -104,6 +132,4 @@ done
 # retrieve the stash to ensure the final commit will be faithfull to synergy
 git stash pop > /dev/null
 
-rm /tmp/subadd.$$.tmp
-
-
+rm /tmp/subadd_status.$$.tmp /tmp/subadd_tasks.$$.tmp /tmp/subadd_content.$$.tmp
